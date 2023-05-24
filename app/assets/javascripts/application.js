0b4d4e3e00f02e564d0f32a7583a1d2d811b1f50
German Acierno <gacierno@gmail.com>
2023-05-24 07:32:29 -0300

    add a navigation bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,11 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).on( 'turbolinks:load', function(){
+    $('.ui.dropdown').dropdown();
+} );
